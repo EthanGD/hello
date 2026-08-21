019b93dbdbc9b8a817bdab52208caa36541ee0cf
user <cysong702@outlook.com>
2026-08-21 17:10:58 +0800

    android done

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,6 @@
+const path = require('path');
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 /**
  * Metro configuration
@@ -6,6 +8,22 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const imageMarkerAndroidPath = path.join(
+  __dirname,
+  'node_modules',
+  'react-native-image-marker',
+  'android',
+);
+
+const config = {
+  resolver: {
+    // Ignore transient native build directories created under the dependency on
+    // Windows. Metro may try to watch them while CMake is deleting them.
+    blockList: exclusionList([
+      path.join(imageMarkerAndroidPath, '.cxx'),
+      path.join(imageMarkerAndroidPath, 'build'),
+    ]),
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
